7560b5fb4d92b19d04df9183bd03448ef909f2ed
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-05-20 13:13:16 +0300

    Commit

--- sudoku.js
+++ sudoku.js
@@ -24,7 +24,7 @@ function prettyBoard(board) {
 
 }
 
-// Exports all the functions to use them in another file.
+// Exports all the functions to use them in another file...
 module.exports = {
   solve,
   isSolved,
